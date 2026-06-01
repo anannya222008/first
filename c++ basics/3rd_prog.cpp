5bba70587cb4377b2845066dad1bdaa6fe82ad62
anannya <anannya222008@gmail.com>
2026-06-01 19:49:06 +0530

    3rd prog

---
+++ c++ basics/3rd_prog.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char grade;
+    cout <<"Enter your grade:";
+    cin>> grade;
+    cout<<"Your grade is "<< grade<<endl;
+    return 0;
+    
+}
