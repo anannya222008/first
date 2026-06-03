381e7626928b9712c846cd5e33789c6497b8ed09
anannya <anannya222008@gmail.com>
2026-06-03 15:30:03 +0530

    code on logical operation

---
+++ c++ basics/7th_prog.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout  << (3 < 4)<< endl; // true
+    cout  << (3 > 4)<< endl; // false
+    cout  << (3 == 4)<< endl; // false
+    cout  << (3 != 4)<< endl; // true
+    return 0;
+}
