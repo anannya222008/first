1ff634d19401641c1f6b9a37c00de979593d5e90
anannya <anannya222008@gmail.com>
2026-06-16 17:06:44 +0530

    uploading pattern problem code

---
+++ c++ basics/02_loops_function_pattern/20_num_pyramid.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=n-i;j>0;j--){
+            cout<<" ";
+        }
+        for(int j=0;j<=i;j++){
+            cout<<i+1<<" ";
+        }
+        
+        cout<<endl;
+    }
+}
+        
